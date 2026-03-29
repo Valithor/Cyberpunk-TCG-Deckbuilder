934f15ff23be72ba129bef9df45dd60905c5d0a5
Valithor <valithor9@gmail.com>
2026-03-30 00:44:18 +0200

    upd actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,7 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ""
-let basePath = "/"
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "")
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  output: "export",
+  basePath: process.env.PAGES_BASE_PATH,
   images: {
     remotePatterns: [
       {
