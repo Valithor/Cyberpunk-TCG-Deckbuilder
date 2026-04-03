78393cb71dafcd51e08c4e6ea1bc6001b2d4a89c
Valithor <valithor9@gmail.com>
2026-04-03 02:41:59 +0200

    added not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,35 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Page not found",
+  description: "The requested page could not be found.",
+
+  robots: {
+    index: false,
+    follow: false,
+  },
+
+  openGraph: {
+    title: "Page not found",
+    description: "The requested page could not be found.",
+    siteName: "Cyberpunk TCG Deckbuilder",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary",
+    title: "Page not found",
+    description: "The requested page could not be found.",
+  },
+}
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center">
+      <h1 className="flex gap-2 flex-col items-center justify-center">
+        <div className="text-3xl text-primary">404</div>
+        <div>Page not found</div>
+      </h1>
+    </div>
+  )
+}
